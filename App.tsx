e8a3130f598593f9685da111a5191938468364c1
Saulo Vallory <me@saulovallory.com>
2019-07-17 20:19:23 -0300

    Initial commit with Expo and  UI Kitten

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider, Layout } from 'react-native-ui-kitten';
+
+const App = () => (
+  <ApplicationProvider
+    mapping={mapping}
+    theme={lightTheme}>
+    <Layout style={{flex: 1}}/>
+  </ApplicationProvider>
+);
+
+export default App;
